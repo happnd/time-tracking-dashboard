aa45b270beabd5f6d762bed038a16635500287b3
whp <example>
2022-07-15 18:37:45 +0200

    app js added & fixes

--- app.js
+++ app.js
@@ -1,59 +1,80 @@
-// <!-- daily -->
-//             32hrs
-//             <!-- weekly -->
-//             Previous - 36hrs
-//             <!-- weekly -->
-//             103hrs
-//             <!-- monthly -->
-//             Previous - 128hrs
-//             <!-- monthly -->
+import data from "./data.js";
 
-//             play
-//             10hrs
-//         <!-- weekly -->
-//         Previous - 8hrs
-//         <!-- weekly -->
-//         23hrs
-//         <!-- monthly -->
-//         Previous - 29hrs
-//         <!-- monthly -->
+const menu = document.querySelector(".menu__nav");
+const dailyBtn = document.querySelector("#dailyBtn");
+const weeklyBtn = document.querySelector("#weeklyBtn");
+const monthlyBtn = document.querySelector("#monthlyBtn");
 
-//         study
-//         4hrs
-//         <!-- weekly -->
-//         Previous - 7hrs
-//         <!-- weekly -->
-//         13hrs
-//         <!-- monthly -->
-//         Previous - 19hrs
-//         <!-- monthly -->
+const scores = document.querySelectorAll(".comp__score");
+const previousScores = document.querySelectorAll(".comp__previous");
+let state = "daily";
 
-//         exercise
-//         4hrs
-//         <!-- weekly -->
-//         Previous - 5hrs
-//         <!-- weekly -->
-//         11hrs
-//         <!-- monthly -->
-//         Previous - 18hrs
-//         <!-- monthly -->
+dailyBtn.classList.toggle("active__button");
 
-//         social
-//         5hrs
-//         <!-- weekly -->
-//         Previous - 10hrs
-//         <!-- weekly -->
-//         21hrs
-//         <!-- monthly -->
-//         Previous - 23hrs
-//         <!-- monthly -->
+console.log(data);
 
-//         selfcare
-//         2hrs
-//         <!-- weekly -->
-//         Previous - 2hrs
-//         <!-- weekly -->
-//         7hrs
-//         <!-- monthly -->
-//         Previous - 11hrs
-//         <!-- monthly -->
+menu.addEventListener("click", (event) => {
+  dailyBtn.classList.remove("active__button");
+  weeklyBtn.classList.remove("active__button");
+  monthlyBtn.classList.remove("active__button");
+
+  if (event.target === dailyBtn) {
+    if (state !== "daily") {
+      scores.forEach((score, index) => {
+        score.innerText = `${data[index].timeframes.daily.current}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      previousScores.forEach((score, index) => {
+        score.innerText = `Yesterday - ${data[index].timeframes.daily.previous}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      dailyBtn.classList.toggle("active__button");
+      state = "daily";
+    }
+  }
+
+  if (event.target === weeklyBtn) {
+    if (state !== "weekly") {
+      scores.forEach((score, index) => {
+        score.innerText = `${data[index].timeframes.weekly.current}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      previousScores.forEach((score, index) => {
+        score.innerText = `Last Week - ${data[index].timeframes.weekly.previous}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      weeklyBtn.classList.toggle("active__button");
+      state = "weekly";
+    }
+  }
+
+  if (event.target === monthlyBtn) {
+    if (state !== "monthly") {
+      scores.forEach((score, index) => {
+        score.innerText = `${data[index].timeframes.monthly.current}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      previousScores.forEach((score, index) => {
+        score.innerText = `Last Month - ${data[index].timeframes.monthly.previous}hrs`;
+        animateCSS(score, "bounceIn");
+      });
+      monthlyBtn.classList.toggle("active__button");
+      state = "monthly";
+    }
+  }
+});
+
+const animateCSS = (element, animation, prefix = "animate__") =>
+  new Promise((resolve) => {
+    const animationName = `${prefix}${animation}`;
+
+    element.classList.add(`${prefix}animated`, animationName);
+
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      element.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    element.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
